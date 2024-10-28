2eb43887483f767c3a924d2625c229196158cb2a
Shivendra Raghuvanshi <developer.raghuvanshi@gmail.com>
2024-10-28 22:46:12 +0530

    Added favicon icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,15 @@ export const metadata: Metadata = {
   title: "Develop with Shiven",
   description:
     "Develop with Shiven is an initiative towards many causes such as Online IT Courses, Daily Blog, AI Literacy and Mastery",
+  icons: {
+    icon: "/favicon.ico", // default favicon
+    shortcut: "/favicon.ico", // shortcut icon for browsers
+    apple: "/apple-touch-icon.png", // for Apple devices
+    other: [
+      { rel: "icon", url: "/favicon-32x32.png", sizes: "32x32" }, // additional sizes
+      { rel: "icon", url: "/favicon-16x16.png", sizes: "16x16" },
+    ],
+  },
 };
 
 export default function RootLayout({
